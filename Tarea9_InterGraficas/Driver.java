f81cccde90799752abaa8204f0003265935bf86a
RodolfoGuerreroMendoza <125853940+RodolfoGuerreroMendoza@users.noreply.github.com>
2023-04-30 16:11:11 -0600

    Add files via upload

---
+++ Tarea9_InterGraficas/Driver.java
@@ -0,0 +1,9 @@
+package Tarea9_InterGraficas;
+
+import Tarea9_InterGraficas.usuario.Principal;
+
+public class Driver {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
