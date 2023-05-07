55643a59c4e662da10c611d4ff20af3696eed902
RodolfoGuerreroMendoza <125853940+RodolfoGuerreroMendoza@users.noreply.github.com>
2023-05-07 16:20:58 -0600

    Add files via upload

---
+++ Extra3_Layouts/Main.java
@@ -0,0 +1,8 @@
+package Extra3_Layouts;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana("Layouts");
+        VentanaGrid ventanaGrid = new VentanaGrid("Ventana Layout cuadricula");
+    }
+}
