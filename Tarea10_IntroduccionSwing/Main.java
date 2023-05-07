4eccf87d929b19058e83e3770e26db648ef7a47a
RodolfoGuerreroMendoza <125853940+RodolfoGuerreroMendoza@users.noreply.github.com>
2023-05-07 16:21:52 -0600

    Add files via upload

---
+++ Tarea10_IntroduccionSwing/Main.java
@@ -0,0 +1,10 @@
+package Tarea10_IntroduccionSwing;
+
+import Extra3_Layouts.Ventana;
+import Extra3_Layouts.VentanaGrid;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaSwing ventanaSwing = new VentanaSwing("Centigrados a Farenheid");
+    }
+}
